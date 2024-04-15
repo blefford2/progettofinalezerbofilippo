f60496632aa1d1ef01065ed82d8009e452749e96
blefford2 <157392100+blefford2@users.noreply.github.com>
2024-04-15 12:57:18 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Header from './common/Header/Header';
+import Footer from './common/Footer/Footer';
+import Home from './common/pages/Home/Home';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Home />
+      <Footer />
+      </div>
+  );
+}
+
+export default App;
---
+++ src/common/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+import './Footer.css'
+
+const Footer = () => {
+    return(
+        <footer id="footer">
+        <div vclassname= "wrapper">
+        
+        </div>
+        </footer>
+    )
+}
+
+export default Footer
